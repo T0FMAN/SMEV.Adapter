c9e5eb27aaec760054090f3665ba6ab1eb475abf
Valeriy <valeriy.elykomov@gmail.com>
2023-10-06 16:16:45 +0500

    Add README.md and LICENSE.txt.

---
+++ SMEV.Adapter/IMessageExchange.cs
@@ -0,0 +1,9 @@
+﻿namespace SMEV.Adapter
+{
+    public interface IMessageExchange
+    {
+        Task<string> Send();
+        Task<string> Get();
+        Task<string> Find();
+    }
+}
---
+++ SMEV.Adapter/MessageExchange.cs
@@ -0,0 +1,20 @@
+﻿namespace SMEV.Adapter
+{
+    public class MessageExchange : IMessageExchange
+    {
+        public Task<string> Find()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> Send()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
