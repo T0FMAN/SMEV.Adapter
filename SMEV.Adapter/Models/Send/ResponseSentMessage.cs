08832cb991beb538d1cec3eac79f5c6a071cebd5
Valeriy <valeriy.elykomov@gmail.com>
2023-10-17 17:50:35 +0500

    Renamed and documented model ResponseSentMessage

---
+++ SMEV.Adapter/Models/Send/ResponseSentMessage.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+
+namespace SMEV.Adapter.Models.Send
+{
+    /// <summary>
+    /// Класс для десериализации ответа от адаптера по методу <c>/send</c>
+    /// </summary>
+    public sealed class ResponseSentMessage
+    {
+        /// <summary>
+        /// Мнемоника ИС
+        /// </summary>
+        [JsonProperty("itSystem")]
+        public string ItSystem { get; set; } = default!;
+        /// <summary>
+        /// ID сообщения
+        /// </summary>
+        [JsonProperty("messageId")]
+        public string MessageId { get; set; } = default!;
+    }
+}
