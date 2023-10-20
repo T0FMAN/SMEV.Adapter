fcab6983b0964e399e8ecb8260d206304bfa281e
Valeriy <valeriy.elykomov@gmail.com>
2023-10-20 14:29:52 +0500

    Added custom exception class InvalidArgumentSendedMessageexception

---
+++ SMEV.Adapter/Exceptions/InvalidArgumentSendedMessageException.cs
@@ -0,0 +1,15 @@
+﻿namespace SMEV.Adapter.Exceptions
+{
+    [Serializable]
+    public class InvalidArgumentSendedMessageException : Exception
+    {
+        public InvalidArgumentSendedMessageException() 
+            : base(string.Format("Объект не соответсвует какой либо из модели отправляемого сообщения"))
+        { 
+        }
+
+        public InvalidArgumentSendedMessageException(Type type) : base(string.Format("Invalid model type: {0}", type.Name)) 
+        { 
+        }
+    }
+}
