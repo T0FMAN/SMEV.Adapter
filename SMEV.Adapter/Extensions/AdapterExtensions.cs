baa5c6e062f78f503b245d728f78699dda2b9651
Valeriy <valeriy.elykomov@gmail.com>
2023-10-23 16:02:01 +0500

    Moved and renamed AdapterHelper => AdapterExtensions

--- SMEV.Adapter/Extensions/AdapterExtensions.cs
+++ SMEV.Adapter/Extensions/AdapterExtensions.cs
@@ -1,29 +1,16 @@
 ﻿using Newtonsoft.Json;
 using SMEV.Adapter.Enums;
-using SMEV.Adapter.Extensions;
+using SMEV.Adapter.Helpers;
 using SMEV.Adapter.Models.Find;
 using SMEV.Adapter.Models.Send;
 
-namespace SMEV.Adapter.Helpers
+namespace SMEV.Adapter.Extensions
 {
-    /// <summary>
-    /// Помощник по адаптеру
-    /// </summary>
-    public static class AdapterHelper
+    internal static class AdapterExtensions
     {
-        /// <summary>
-        /// Адрес веб-сервиса адапатера, включая его контекстный путь. 
-        /// Например, <c>http://localhost:7590/ws</c>
-        /// </summary>
-        public static string Address = "http://localhost:7590/ws";
-        /// <summary>
-        /// Мнемоника ИС, от которой исходит запрос / ответ
-        /// </summary>
-        public static string MnemonicIS;
-
         internal static async Task<Model> ExecuteRequestToSmev<Model>(this HttpClient httpClient, EndpointAdapter endpoint, object messageData)
         {
-            var uri = new Uri(@$"{Address}/{endpoint}");
+            var uri = new Uri(@$"{httpClient.BaseAddress}/{endpoint}");
 
             var json = JsonConvert.SerializeObject(messageData);
 
