4aa67119a9189eed06b18e533b0498d47408ed71
Valeriy <valeriy.elykomov@gmail.com>
2023-10-26 09:39:40 +0500

    Create GuidHelper.cs
    Added guid helper

---
+++ SMEV.Adapter/Helpers/GuidHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace SMEV.Adapter.Helpers
+{
+    internal static class GuidHelper
+    {
+        internal static string GetUniqueId()
+        {
+            var guid = Guid.NewGuid();
+
+            return guid.ToString();
+        }
+    }
+}
