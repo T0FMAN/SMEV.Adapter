46e72cc9ee6c18f5fbf41ecb9cd468ed9cee810f
Valeriy <valeriy.elykomov@gmail.com>
2023-10-27 13:59:35 +0500

    Update Status.cs
    Added documentation

--- SMEV.Adapter/Models/MessageContent/Status.cs
+++ SMEV.Adapter/Models/MessageContent/Status.cs
@@ -2,10 +2,19 @@
 
 namespace SMEV.Adapter.Models.MessageContent
 {
+    /// <summary>
+    /// Статус сообщения
+    /// </summary>
     public sealed class Status
     {
+        /// <summary>
+        /// Код статуса
+        /// </summary>
         [JsonProperty("code")]
         public string Code { get; set; }
+        /// <summary>
+        /// Описание статуса сообщения
+        /// </summary>
         [JsonProperty("description")]
         public string Description { get; set; }
     }
