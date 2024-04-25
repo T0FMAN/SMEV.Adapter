5a181504633f13ea465e378b31582284bff1d1ea
Valeriy <valeriy.elykomov@gmail.com>
2024-04-25 15:56:04 +0500

    Update HttpResponseMessageExtensions.cs

--- SMEV.Adapter/Extensions/HttpResponseMessageExtensions.cs
+++ SMEV.Adapter/Extensions/HttpResponseMessageExtensions.cs
@@ -1,7 +1,5 @@
-﻿using Newtonsoft.Json;
-using SMEV.Adapter.Exceptions;
+﻿using SMEV.Adapter.Exceptions;
 using System.Runtime.CompilerServices;
-using System.Text.Json.Serialization;
 
 namespace SMEV.Adapter.Extensions
 {
