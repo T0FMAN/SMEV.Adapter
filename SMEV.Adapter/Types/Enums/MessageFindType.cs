c621374849d3edc612254ebacced6969d44fa111
Valeriy <valeriy.elykomov@gmail.com>
2024-05-06 17:59:20 +0500

    Create MessageFindType.cs

---
+++ SMEV.Adapter/Types/Enums/MessageFindType.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace SMEV.Adapter.Types.Enums
+{
+    /// <summary>
+    /// Тип сообщения для поиска в очереди сообщения
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum MessageFindType
+    {
+        /// <summary>
+        /// Запрос
+        /// </summary>
+        [EnumMember(Value = "REQUEST")]
+        Request,
+        /// <summary>
+        /// Ответ
+        /// </summary>
+        [EnumMember(Value = "RESPONSE")]
+        Response
+    }
+}
