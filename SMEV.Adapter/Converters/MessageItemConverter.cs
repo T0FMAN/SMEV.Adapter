7f36d6188e0428e44c3804d2648582805ca07c04
Valeriy <valeriy.elykomov@gmail.com>
2024-05-15 11:17:08 +0500

    Update MessageItemConverter.cs

--- SMEV.Adapter/Converters/MessageItemConverter.cs
+++ SMEV.Adapter/Converters/MessageItemConverter.cs
@@ -1,11 +1,11 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
-using SMEV.Adapter.XsdTypes;
+using SMEV.Adapter.Types;
 
 namespace SMEV.Adapter.Converters
 {
-    public class MessageItemConverter : CustomCreationConverter<IMessage>
+    internal class MessageItemConverter : CustomCreationConverter<IMessage>
     {
         public override IMessage Create(Type objectType)
         {
@@ -14,14 +14,14 @@ namespace SMEV.Adapter.Converters
 
         public IMessage Create(Type objectType, JObject jObject)
         {
-            var type = (string)jObject.Property("messageType");
+            var type = (string)jObject.Property("messageType")!;
 
             switch (type)
             {
                 case "ResponseMessageType":
-                    return new ResponseMessageType();
+                    return new ResponseMessage();
                 case "RequestMessageType":
-                    return new RequestMessageType();
+                    return new RequestMessage();
             }
 
             throw new ApplicationException(string.Format("The message type {0} is not supported!", type));
