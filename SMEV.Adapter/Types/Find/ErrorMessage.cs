06c183d270e4db27ffd84dfc541e560c401dd82f
Valeriy <valeriy.elykomov@gmail.com>
2024-05-22 17:25:46 +0500

    Create ErrorMessage and Fault types

---
+++ SMEV.Adapter/Types/Find/ErrorMessage.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using SMEV.Adapter.Types.Enums;
+
+namespace SMEV.Adapter.Types.Find
+{
+    /// <summary>
+    /// Сообщение ошибки
+    /// </summary>
+    public class ErrorMessage : StatusMessage
+    {
+        /// <summary>
+        /// <para>Тип источника ошибки.</para>
+        /// </summary>
+        [JsonProperty("type")]
+        public ErrorType Type { get; set; }
+
+        /// <summary>
+        /// <para>Сведения об ошибке.</para>
+        /// </summary>
+        [JsonProperty("fault")]
+        public Fault Fault { get; set; } = default!;
+    }
+}
---
+++ SMEV.Adapter/Types/Find/Fault.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace SMEV.Adapter.Types.Find
+{
+    /// <summary>
+    /// Данные ошибки
+    /// </summary>
+    public class Fault
+    {
+        /// <summary>
+        /// <para>Код ошибки, генерируемый Адаптером СМЭВ либо возвращаемый СМЭВ 3</para>
+        /// </summary>
+        [JsonProperty("code")]
+        public string? Code { get; set; }
+
+        /// <summary>
+        /// <para>Описание ошибки, генерируемое Адаптером СМЭВ либо возвращаемое СМЭВ 3</para>
+        /// </summary>
+        [JsonProperty("description")]
+        public string? Description { get; set; }
+    }
+}
